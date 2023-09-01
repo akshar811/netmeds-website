ef6cc9667bf9ef304c9ffa9111462e7d734dfbb5
yash <officialyashvora1978@gmail.com>
2023-09-01 19:12:32 +0530

    yash

--- components/nav.js
+++ components/nav.js
@@ -1,10 +1,10 @@
 const navbar = () =>{
     return `
     
-    <header class="navbar position-sticky navbar-expand-md">
+    <header class="position-sticky navbar-expand-md">
         <div class="container d-flex align-items-center justify-content-between">
             <div>
-                <a href="index.html"><img src="https://nms-assets.s3-ap-south-1.amazonaws.com/images/cms/aw_rbslider/slides/1663609483_netmeds-new-logo.svg" class="logo"></a>
+                <a href="/index.html"><img src="https://nms-assets.s3-ap-south-1.amazonaws.com/images/cms/aw_rbslider/slides/1663609483_netmeds-new-logo.svg" class="logo"></a>
             </div>
             <div class="search-bar">
                 <form action="">
@@ -23,7 +23,7 @@ const navbar = () =>{
                     <li class="nav-item px-2"><a href="#" class="nav-link text-light"><i class="bi bi-tag"></i>
                             Offers</a></li>
                     <li class="nav-item px-2"><a href="pages/cart.html" class="nav-link text-light"><i
-                                class="bi bi-cart3"></i>
+                                    class="bi bi-cart3"></i>
                             Cart<a></li>
                     <li class="nav-item px-2"><a href="pages/login.html" class="nav-link text-light"><i
                                 class="bi bi-person-circle"></i> Sign in / Sign up</a></li>
@@ -35,13 +35,13 @@ const navbar = () =>{
 
     <nav>
         <div class="sub-menu pb-1">
-            <ul class="d-flex justify-content-evenly">
+            <ul class="container d-flex justify-content-evenly">
                 <div class="d-flex align-items-center">
                     <div class="me-2">
                         <img src="https://www.netmeds.com/assets/version1693541495/gloryweb/images/icons/medicine.svg" class="">
                     </div>
                     <div>
-                        <li class="text-light"><a href="pages/ayurvedic.html"
+                        <li class="text-light"><a href="/pages/ayurvedic.html"
                             class="text-decoration-none text-light">Aayurvedic</a> </li>
                     </div>
                 </div>
@@ -50,7 +50,7 @@ const navbar = () =>{
                         <img src="https://www.netmeds.com/assets/gloryweb/images/icons/wellness.svg" class="">
                     </div>
                     <div>
-                        <li class="text-light"><a href="pages/details.html"
+                        <li class="text-light"><a href="/pages/details.html"
                             class="text-decoration-none text-light">Details</a> </li>
                     </div>
                 </div>
@@ -59,7 +59,7 @@ const navbar = () =>{
                         <img src="https://www.netmeds.com/assets/gloryweb/images/icons/diagnostics.svg" class="">
                     </div>
                     <div>
-                        <li class="text-light"><a href="pages/homeopathey.html"
+                        <li class="text-light"><a href="/pages/homeopathey.html"
                             class="text-decoration-none text-light">Homeopathy</a> </li>
                     </div>
                 </div>
@@ -68,7 +68,7 @@ const navbar = () =>{
                         <img src="https://www.netmeds.com/assets/gloryweb/images/icons/wellness.svg" class="">
                     </div>
                     <div>
-                        <li class="text-light"><a href="pages/eyewear.html"
+                        <li class="text-light"><a href="/pages/eyewear.html"
                             class="text-decoration-none text-light">Eyewear</a> </li>
                     </div>
                 </div>
@@ -77,7 +77,7 @@ const navbar = () =>{
                         <img src="https://www.netmeds.com/assets/version1693541495/gloryweb/images/icons/health-library.svg" class="">
                     </div>
                     <div>
-                        <li class="text-light"><a href="pages/veterinary.html"
+                        <li class="text-light"><a href="/pages/veterinary.html"
                             class="text-decoration-none text-light">Veterinary</a> </li>
                     </div>
                 </div>
