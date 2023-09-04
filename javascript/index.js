4c4a5451363073c1e7d84fd83eb987d7fe184505
yash <officialyashvora1978@gmail.com>
2023-09-04 10:50:30 +0530

    count

--- javascript/index.js
+++ javascript/index.js
@@ -3,3 +3,36 @@ document.getElementById("nav").innerHTML = navbar()
 
 import footer from "../components/footer.js";
 document.getElementById("footer").innerHTML = footer()
+
+let count = 17
+let timeout = setInterval(() => {
+    count--
+
+    document.getElementById("c1").innerHTML = count
+
+    if (count == 0) {
+        clearInterval(timeout)
+    }
+},100000);
+
+let count1 = 17
+let timeout1 = setInterval(() => {
+    count1--
+
+    document.getElementById("c2").innerHTML = count1
+
+    if (count1 == 0) {
+        clearInterval(timeout1)
+    }
+},60000);
+
+let count2 = 60
+let timeout2 = setInterval(() => {
+    count2--
+
+    document.getElementById("c3").innerHTML = count2
+
+    if (count2 == 0) {
+        clearInterval(timeout2)
+    }
+},1000);
